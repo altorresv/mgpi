7407a20c90d1cb07f035442880b602b047ded56a
Usuario <Usuario@Cicom01>
2019-06-30 09:31:24 -0500

    version tres

--- alix/src/alix/Alix.java
+++ alix/src/alix/Alix.java
@@ -17,7 +17,7 @@ public class Alix {
     public static void main(String[] args) {
         System.out.println("hola");
         System.out.println("version 2");
-        System.out.println("version 3");
+        System.out.println("version tres");
     }
     
 }
